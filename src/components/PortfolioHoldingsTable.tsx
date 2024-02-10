5afc17188e8ad99b0ccb1f1e723f67220c23609a
funnlsolutions <funnlsolutions@gmail.com>
2024-02-10 10:37:11 -0500

    working on table

--- src/components/PortfolioHoldingsTable.tsx
+++ src/components/PortfolioHoldingsTable.tsx
@@ -1,23 +1,65 @@
 import React, { useContext } from 'react';
+import { ChevronUpDownIcon } from "@heroicons/react/24/outline";
+import { Card, CardBody, Typography } from "@material-tailwind/react";
+import { currencyFormatter } from '../utils/formatting';
 import { PortfolioContext } from '../context/PortfolioContext';
+import { Holding } from '../context/types';
+import { ArrowDownIcon, ArrowUpIcon } from '@heroicons/react/16/solid';
 
-const PortfolioHoldingsTable: React.FC = () => {
+const TABLE_HEAD = ["Symbol", "Shares", "Purchase Price", "Current Price", "Change (1d)", "Holding Gain", "Holding Value", "Sector"];
+
+export const SortableTable: React.FC = () => {
     const { holdings } = useContext(PortfolioContext);
-
-    console.log(holdings);
-
     return (
-        <>
-            {holdings.map((holding) => (
-                <div key={holding.symbol}>
-                    <p>Symbol: {holding.symbol}</p>
-                    <p>Current Price: {holding.currentPrice}</p>
-                    <p>Current Value: {holding.currentValue}</p>
-                    {/* Render other holding details as needed */}
-                </div>
-            ))}
-        </>
-    )
-}
+        <div className="dashboard-item-wrapper">
+            <Card className="h-full w-full">
+                <CardBody className="overflow-scroll px-0">
+                    <table className="mt-4 w-full min-w-max table-auto text-left">
+                        <thead>
+                            <tr>
+                            {TABLE_HEAD.map((head) => (
+                                <th key={head} className="border-y border-slate-800 py-3 px-2">
+                                    <Typography variant="small" color="blue-gray" className="flex items-center justify-between gap-2 dark:text-almost-white">
+                                        {head} <ChevronUpDownIcon strokeWidth={2} className="h-4 w-4 dark:text-slate-600" />
+                                    </Typography>
+                                </th>
+                            ))}
+                            </tr>
+                        </thead>
 
-export default PortfolioHoldingsTable;
+                        <tbody>
+                            {holdings.map((holding: Holding) => (
+                                <>
+                                    {
+                                        (() => {
+                                            const PerformanceIcon = holding.change > 0 ? ArrowUpIcon : ArrowDownIcon;
+                                            const performanceIconTextColor = PerformanceIcon === ArrowUpIcon ? 'text-green-600' : 'text-red-900';
+                                            const performanceTextColor = PerformanceIcon === ArrowUpIcon ? 'text-green-400' : 'text-red-600';
+                                            return (
+                                                <tr key={holding.symbol}>
+                                                    <td className="py-3 px-2 dark:text-slate-200 font-bold">{holding.symbol}</td>
+                                                    <td className="py-3 px-2 dark:text-slate-300">{holding.numberOfShares}</td>
+                                                    <td className="py-3 px-2 dark:text-slate-300">${holding.purchasePrice}</td>
+                                                    <td className="py-3 px-2 dark:text-slate-300">${holding.currentPrice}</td>
+                                                    <td className={`py-3 px-2 flex items-center gap-2 ${performanceTextColor}`}>
+                                                        <PerformanceIcon className={`h-5 w-5 ${performanceIconTextColor}`} />
+                                                        {holding.change}%
+                                                    </td>
+                                                    <td className="py-3 px-2 dark:text-slate-300 font-bold">demo</td>
+                                                    <td className="py-3 px-2 dark:text-slate-300 font-bold">{currencyFormatter.format(holding.currentValue)}</td>
+                                                    <td className="py-3 px-2 dark:text-slate-300">{holding.sector}</td>
+                                                </tr>
+                                            );
+                                        })()
+                                    }
+                                </>
+                            ))}
+                        </tbody>
+                    </table>
+                </CardBody>
+            </Card>
+        </div>
+    );
+};
+
+export default SortableTable;
--- src/containers/PortfolioHoldings.tsx
+++ src/containers/PortfolioHoldings.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import PortfolioHoldingsTable from "../components/PortfolioHoldingsTable";
-import EnhancedTable from "../components/SortableTable";
+import { SortableTable } from "../components/PortfolioHoldingsTable";
 
 const PortfolioHoldings: React.FC = () => {
     return (
         <div className="dashboard-item min-w-full">
             <h2 className="text-md text-white font-medium text-bold mb-1">Portfolio Holdings</h2>
-            {/* <PortfolioHoldingsTable /> */}
-            <EnhancedTable />
+            <SortableTable />
         </div>
     )
 }
